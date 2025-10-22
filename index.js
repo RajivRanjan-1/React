645c75b80538439667c3284edc11ca60e5f1c5bd
Rajiv Ranjan <rajivranjan28122004@gmail.com>
2025-10-22 20:42:59 +0530

    Assignment_3

--- index.js
+++ index.js
@@ -1,20 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './style.css';
 
-const heading = React.createElement("h1",{id:"heading"},"hello world");
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// const heading = React.createElement("div",{className:"title"},
+//     [   React.createElement("h1",{id:'h1'},'h1'),
+//         React.createElement("h2",{id:'h2'},'h2'),
+//         React.createElement("h3",{id:'h3'},'h3')  ]
+// );
 
-const Heading = <h1 id="heading">React (JSX Syntax)</h1>;
+// const nestedheading = (
+//     <div className="title">
+//         <h1>h1</h1>
+//         <h2>h2</h2>
+//         <h3>h3</h3>
+//     </div>
+// );
 
-const HeadingComponent = () => {
-    // {heading},
+// const Nestedhead = () => (
+//     <div className="head">
+//         <h1>h1</h1>
+//         <h2>h2</h2>
+//         <h3>h3</h3>
+//     </div>
+// );
+
+// const Nestedheading = () => (
+//     <div className="title">
+//         {/* {nestedheading}
+//         <Nestedhead></Nestedhead>
+//         <Nestedhead/>
+//         {Nestedhead()} */}
+//         <h1 className='mainheading'>h1</h1>
+//         <h2>h2</h2>
+//         <h3>h3</h3>
+//     </div>
+// );
+
+const HeaderComponent = () => {
     return (
-        <div className='container'>
-            {Heading}
-            <h1 id="heading">React(HeadingComponent)</h1>
-        </div>
-    );
+    <header>
+      <nav>
+        <img src="./icon.png" alt="Logo" className="img-logo" />
+        <input type="text" className="input" placeholder="Type something..." />
+        <img src="./profile.png" alt="" className="img-profile" />
+      </nav>
+    </header>
+  );
 };
 
-// root.render(Heading);
-root.render(<HeadingComponent/>);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(heading);
+// root.render(nestedheading);
+// root.render(<Nestedheading/>);
+
+root.render(<HeaderComponent/>);
